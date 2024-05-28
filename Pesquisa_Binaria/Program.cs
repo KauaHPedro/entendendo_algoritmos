81324c6c77bd38e9f9ed39407e70a8abba249b47
Kauã Pedro <kauahcpedro@hotmail.com>
2024-05-28 00:02:10 -0300

    Implementando pesquisa binária

--- Pesquisa_Binaria/Program.cs
+++ Pesquisa_Binaria/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] array = {1, 2, 6, 9, 15, 18, 24, 28, 35, 42, 78 };
+            int aux = pesquisa_binaria(array, 19);
+
+            if (aux != -1)
+            {
+                Console.WriteLine($"Número encontrado na posição {aux} do vetor!");
+            } else
+            {
+                Console.WriteLine("Número não encontrado no vetor");
+            }
+        }
+
+        public static int pesquisa_binaria(int[] array, int item)
+        {
+            int baixo = 0;
+            int alto = array.Length -1;
+
+            while (baixo <= alto)
+            {
+                int meio = (baixo + alto) / 2;
+                int palpite = array[meio];
+
+                if (palpite == item)
+                {
+                    return meio;
+                }
+                if (palpite > item)
+                {
+                    alto = meio - 1;
+                }
+                else
+                {
+                    baixo = meio + 1;
+                }
+            }
+            return -1;
         }
     }
 }
--- Pesquisa_Binaria/obj/Debug/net8.0/Pesquisa_Binaria.AssemblyInfo.cs
+++ Pesquisa_Binaria/obj/Debug/net8.0/Pesquisa_Binaria.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pesquisa_Binaria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c059d0e2c5e0786658eaedbf63a1c05b6b6e4768")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pesquisa_Binaria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pesquisa_Binaria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
