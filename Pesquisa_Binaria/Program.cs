c059d0e2c5e0786658eaedbf63a1c05b6b6e4768
Kauã Pedro <kauahcpedro@hotmail.com>
2024-05-27 23:30:17 -0300

    Iniciando repo

---
+++ Pesquisa_Binaria/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Pesquisa_Binaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
